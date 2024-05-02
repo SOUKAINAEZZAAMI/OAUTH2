d3331738dde14a88c587e5859655796ef195c337
ezzaami soukaina <ezzaamisoukaina@gmail.com>
2024-05-02 15:03:13 +0100

    01

---
+++ src/main/java/oauth2/oauth2googlegithub/Oauth2GoogleGithubApplication.java
@@ -0,0 +1,13 @@
+package oauth2.oauth2googlegithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2GoogleGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2GoogleGithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oauth2/oauth2googlegithub/Oauth2GoogleGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package oauth2.oauth2googlegithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2GoogleGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
